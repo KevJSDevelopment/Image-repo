2c2036a5250db21a0ecb628dab8220bf53f510ec
KevJSDevelopment <Kshhn12@gmail.com>
2021-05-07 11:46:13 -0400

    base funcitons for app working

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,8 @@
 class Upload < ApplicationRecord
     has_one_attached :image, dependent: :destroy
 
+    validates :image, attached: true, content_type: { in: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'], message: 'files only, must be one of the following file types: jpg, jpeg, png, or gif'}
+    
     def self.search(search)
         @uploads = Upload.where("title LIKE '%#{search}%'")
     end
